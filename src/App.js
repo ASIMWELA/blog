bcaf02b725b8408c6868389dba756e7630f84f2b
ASIMWELA <augastinesimwela@gamil.com>
2021-03-11 00:53:53 +0200

    project skeleton

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {Switch, Route} from 'react-router-dom';
+
+//import components
+import {  Home, 
+          NavBar,
+          Profile,
+          Projects,
+          Contact,
+          Login,
+          } from './components/commons'
 
 function App() {
+  const RoutesWithNavBar = ()=>{
+    return (
+      <div>
+        <NavBar/>
+        <Switch>
+          <Route exact path='/' component={Home}/>
+          <Route  path='/profile' component={Profile}/>
+          <Route  path='/projects' component={Projects}/> 
+          <Route  path='/contact' component={Contact}/> 
+          <Route  path='/contact' component={Home}/> 
+        </Switch>
+      </div>
+    )
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Switch>
+          <Route path='/login' component={Login}/>
+          <Route component={RoutesWithNavBar}/>
+      </Switch>
+    </>
   );
 }
 
